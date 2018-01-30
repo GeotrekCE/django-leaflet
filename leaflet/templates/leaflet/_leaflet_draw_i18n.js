f07f3aa08b5399208bafd4e900dd2537ee2d6f7f
Gaël UTARD <gael.utard@makina-corpus.com>
2018-01-30 17:26:29 +0100

    Update Leaflet.draw translations

--- leaflet/templates/leaflet/_leaflet_draw_i18n.js
+++ leaflet/templates/leaflet/_leaflet_draw_i18n.js
@@ -3,55 +3,66 @@ L.drawLocal = {
     draw: {
         toolbar: {
             actions: {
-                title: "{% trans "Cancel drawing" %}",
-                text: "{% trans "Cancel" %}"
+                title: '{% trans "Cancel drawing" %}',
+                text: '{% trans "Cancel" %}'
+            },
+            finish: {
+                title: '{% trans "Finish drawing" %}',
+                text: '{% trans "Finish" %}'
             },
             undo: {
-                title: "{% trans "Delete last point drawn" %}",
-                text: "{% trans "Delete last point" %}"
+                title: '{% trans "Delete last point drawn" %}',
+                text: '{% trans "Delete last point" %}'
             },
             buttons: {
-                polyline: "{% trans "Draw a polyline" %}",
-                polygon: "{% trans "Draw a polygon" %}",
-                rectangle: "{% trans "Draw a rectangle" %}",
-                circle: "{% trans "Draw a circle" %}",
-                marker: "{% trans "Draw a marker" %}"
+                polyline: '{% trans "Draw a polyline" %}',
+                polygon: '{% trans "Draw a polygon" %}',
+                rectangle: '{% trans "Draw a rectangle" %}',
+                circle: '{% trans "Draw a circle" %}',
+                marker: '{% trans "Draw a marker" %}',
+                circlemarker: '{% trans "Draw a circlemarker" %}'
             }
         },
         handlers: {
             circle: {
                 tooltip: {
-                    start: "{% trans "Click and drag to draw circle." %}"
+                    start: '{% trans "Click and drag to draw circle." %}'
+                },
+                radius: '{% trans "Radius" %}'
+            },
+            circlemarker: {
+                tooltip: {
+                    start: '{% trans "Click map to place circle marker." %}'
                 }
             },
             marker: {
                 tooltip: {
-                    start: "{% trans "Click map to place marker." %}"
+                    start: '{% trans "Click map to place marker." %}'
                 }
             },
             polygon: {
                 tooltip: {
-                    start: "{% trans "Click to start drawing shape." %}",
-                    cont: "{% trans "Click to continue drawing shape." %}",
-                    end: "{% trans "Click first point to close this shape." %}"
+                    start: '{% trans "Click to start drawing shape." %}',
+                    cont: '{% trans "Click to continue drawing shape." %}',
+                    end: '{% trans "Click first point to close this shape." %}'
                 }
             },
             polyline: {
-                error: "{% trans "<strong>Error:</strong> shape edges cannot cross!" %}",
+                error: '{% trans "<strong>Error:</strong> shape edges cannot cross!" %}',
                 tooltip: {
-                    start: "{% trans "Click to start drawing line." %}",
-                    cont: "{% trans "Click to continue drawing line." %}",
-                    end: "{% trans "Click last point to finish line." %}"
+                    start: '{% trans "Click to start drawing line." %}',
+                    cont: '{% trans "Click to continue drawing line." %}',
+                    end: '{% trans "Click last point to finish line." %}'
                 }
             },
             rectangle: {
                 tooltip: {
-                    start: "{% trans "Click and drag to draw rectangle." %}"
+                    start: '{% trans "Click and drag to draw rectangle." %}'
                 }
             },
             simpleshape: {
                 tooltip: {
-                    end: "{% trans "Release mouse to finish drawing." %}"
+                    end: '{% trans "Release mouse to finish drawing." %}'
                 }
             }
         }
@@ -60,31 +71,35 @@ L.drawLocal = {
         toolbar: {
             actions: {
                 save: {
-                    title: "{% trans "Save changes." %}",
-                    text: "{% trans "Save" %}"
+                    title: '{% trans "Save changes" %}',
+                    text: '{% trans "Save" %}'
                 },
                 cancel: {
-                    title: "{% trans "Cancel editing, discards all changes." %}",
-                    text: "{% trans "Cancel" %}"
+                    title: '{% trans "Cancel editing, discards all changes" %}',
+                    text: '{% trans "Cancel" %}'
+                },
+                clearAll:{
+                    title: '{% trans "Clear all layers" %}',
+                    text: '{% trans "Clear All" %}'
                 }
             },
             buttons: {
-                edit: "{% trans "Edit layers" %}",
-                editDisabled: "{% trans "No layers to edit." %}",
-                remove: "{% trans "Delete layers" %}",
-                removeDisabled: "{% trans "No layers to delete." %}"
+                edit: '{% trans "Edit layers" %}',
+                editDisabled: '{% trans "No layers to edit" %}',
+                remove: '{% trans "Delete layers" %}',
+                removeDisabled: '{% trans "No layers to delete" %}'
             }
         },
         handlers: {
             edit: {
                 tooltip: {
-                    text: "{% trans "Drag handles, or marker to edit feature." %}",
-                    subtext: "{% trans "Click cancel to undo changes." %}"
+                    text: '{% trans "Drag handles or markers to edit features." %}',
+                    subtext: '{% trans "Click cancel to undo changes." %}'
                 }
             },
             remove: {
                 tooltip: {
-                    text: "{% trans "Click on a feature to remove" %}"
+                    text: '{% trans "Click on a feature to remove." %}'
                 }
             }
         }
